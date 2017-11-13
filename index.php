833f603ec3a381146f347610f08bbed1d5ed9365
Simon Rasmussen <simon@huebanden.dk>
2017-11-13 13:54:19 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
+    
+    <title></title>
+</head>
+<body>
+<h2>Genbrug 2</h2>
+
+
+
+<div class="row">
+    <div class="col-md-3">
+
+
+    </div>
+    <div class="col-md-6">
+
+
+
+        <form action="test.php" method="post">
+            <div class="form-group">
+                <label for="formGroupExampleInput">Din telefon</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="iPhone 5s, iPhone 7, iPhone X, ETC...">
+
+            </div>
+            <div class="checkbox">
+                <label><input type="checkbox" value="">Telefonen virker</label>
+            </div>
+            <div class="checkbox">
+                <label><input type="checkbox" value="">Skærm</label>
+            </div>
+            <div class="checkbox">
+                <label><input type="checkbox" value="">Kan oplade</label>
+            </div>
+            <button type="submit" class="btn btn-info">Info</button>
+        </form>
+    </div>
+    <div class="col-md-3">.col-md-3</div>
+</div>
+
+
+
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="styles.css">
+    <title></title>
+</head>
+<body>
+<h2>Result</h2>
+
+
+
+<div class="container">
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>Butik</th>
+            <th>Pris</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>
+                <img src="https://www.power.dk/images/logos/powerdk/logo.svg">
+                </td>
+            <td>1338 kr.</td>
+        </tr>
+        <tr>
+            <td>
+                <img src="https://www.elgiganten.dk/INTERSHOP/static/WFS/store-elgigantenDK-Site/-/-/da_DK/images/site/elgigantenDK/logo-large.png?v=3">
+                </td>
+            <td>1337 kr.</td>
+        </tr>
+        <tr>
+            <td>Dooley</td>
+            <td>july@example.com</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
+
+</body>
+</html>
